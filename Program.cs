bcb28cc24c5af785f9282c31983dbb10c082cf25
DSAsefa <danielasefa975@gmail.com>
2024-07-16 22:46:17 -0600

    init commit
    Project Started

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab5_Creating_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Rosie!");
+        }
+    }
+}
